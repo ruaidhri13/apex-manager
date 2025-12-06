6c5f3659f7d14d106b76d1787fec61514258aae9
ruaidhri13 <ruaidhri2006@gmail.com>
2025-12-06 00:09:57 +0000

    initial project setup with spring boot dependencies

---
+++ src/main/java/com/apexmanager/api/ApexManagerApplication.java
@@ -0,0 +1,13 @@
+package com.apexmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apexmanager/api/ApexManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
